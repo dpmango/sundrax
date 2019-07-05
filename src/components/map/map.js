2558e1fe305e8a0370bd51cc12e7cfac081f8168
Sergey Khmelevskoy <xs290@me.com>
2019-07-05 19:35:21 +0300

    maps fixed

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -5,15 +5,15 @@
   APP.Components.Map = {
     init: function() {
       if ($('#contact-map').length > 0) {
-        ymaps.ready(initContactMap);
+        ymaps.ready(this.initContactMap);
       }
     },
     initContactMap: function() {
       var myMap;
 
       myMap = new ymaps.Map('contact-map', {
-        center: [55.753215, 37.622504],
-        zoom: 10,
+        center: [55.724713, 37.645034],
+        zoom: 15,
       });
 
       myMap.controls.remove('trafficControl');
@@ -24,6 +24,19 @@
       myMap.controls.remove('routeEditor');
       myMap.controls.remove('typeSelector');
       // myMap.controls.remove('zoomControl');
+
+      var placeholder = new ymaps.Placemark(
+        [55.724713, 37.645034],
+        {
+          // balloonContent: 'ул. Летниковская, д. 11/10, стр. 1, подъезд 6',
+          iconCaption: 'ул. Летниковская, д. 11/10, стр. 1, подъезд 6',
+        },
+        {
+          preset: 'islands#redCircleDotIconWithCaption',
+        }
+      );
+
+      myMap.geoObjects.add(placeholder);
     },
   };
 })(jQuery, window.APP);
