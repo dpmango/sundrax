48906aeb96925f5133e21d2561fac7f7c77dadc7
Sergey Khmelevskoy <xs290@me.com>
2019-07-25 23:16:46 +0300

    fix selectric

--- src/js/modules/ui/selectric.js
+++ src/js/modules/ui/selectric.js
@@ -10,6 +10,8 @@
       $select.selectric({
         // responsive: true,
         maxHeight: 300,
+        disableOnMobile: false,
+        nativeOnMobile: true,
         // arrowButtonMarkup:
         //   '<b class="button"><svg class="ico ico-select-down"><use xlink:href="img/sprite.svg#ico-select-down"></use></svg></b>',
 
